b1dac20f3136eac66b9498d6b7de44cc5045c469
Rex Low <qiweilow950823@gmail.com>
2016-10-13 09:27:46 +0800

    Setting up

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+class Main extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Setting up</Text>
+      </View>
+    );
+  }
+}
+
+export default Main;
