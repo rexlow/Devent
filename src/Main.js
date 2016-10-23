ff943a68382a1a29164765029a7ef9d9aa7a4637
Rex Low <qiweilow950823@gmail.com>
2016-10-23 18:11:31 +0800

    Set up redux boilerplate

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import {
   View,
   Text,
+  StatusBar
 } from 'react-native';
 
 class Main extends Component {
   render() {
     return (
-      <View>
-        <Text>Setting up</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={styles.mainContainer}>
+          <StatusBar barStyle="light-content"/>
+
+        </View>
+      </Provider>
     );
+  }
+}
+
+const styles = {
+  mainContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.75)'
   }
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import Data from './SampleData';
+
+export default combineReducers({
+  sampleState: Data
+})
