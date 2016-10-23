49e3bc0db9ef8d9470ae7ba1c881865ba752b2a1
Rex Low <qiweilow950823@gmail.com>
2016-10-23 22:15:26 +0800

    Finished touching up login screen

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
-
 import {
   View,
   Text
 } from 'react-native';
 
+import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
+import { Kohana } from 'react-native-textinput-effects';
 
 class Login extends Component {
-  render(){
-    return(
-      <View style={styles.mainContainer}>
 
+  render(){
+    const { mainContainer, upperPart, middlePart, bottomPart, loginText, registerText } = styles;
+    return(
+      <View style={mainContainer}>
+        <View style={upperPart}>
+          <Kohana
+            style={{ backgroundColor: 'transparent', height: 50 }}
+            label={'Email'}
+            iconClass={MaterialsIcon}
+            iconName={'email'}
+            iconColor={'#1976D2'}
+            labelStyle={{ color: '#FFFFFF' }}
+            inputStyle={{ color: '#FFFFFF' }}
+          />
+          <Kohana
+            style={{ backgroundColor: 'transparent', height: 50 }}
+            label={'Password'}
+            iconClass={MaterialsIcon}
+            iconName={'vpn-key'}
+            iconColor={'#1976D2'}
+            labelStyle={{ color: '#FFFFFF' }}
+            inputStyle={{ color: '#FFFFFF' }}
+          />
+        </View>
+        <View style={middlePart}>
+          <Text style={loginText}>Login</Text>
+        </View>
+        <View style={bottomPart}>
+          <Text style={registerText}>Haven't registered?</Text>
+        </View>
       </View>
     )
   }
@@ -21,7 +49,33 @@ const styles = {
     flex: 1,
     backgroundColor: '#448AFF',
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  upperPart: {
+    flex: 6,
+    paddingTop: 20
+  },
+  middlePart: {
+    flex: 3,
+    backgroundColor: '#FF4081',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bottomPart: {
+    flex: 0.5,
+    backgroundColor: '#FF4081',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    padding: 5
+  },
+  loginText: {
+    fontSize: 30,
+    fontWeight: '500',
+    color: '#FFFFFF'
+  },
+  registerText: {
+    fontSize: 18,
+    fontWeight: '400',
+    color: '#FFFFFF'
   }
 }
 
