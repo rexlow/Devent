1c9fb47d127482269b5041bbce6a1346c15d227a
Rex Low <qiweilow950823@gmail.com>
2016-11-07 23:05:26 +0800

    Able to push image to firebase storage now

--- src/containers/tabContainers/Profile.js
+++ src/containers/tabContainers/Profile.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import * as actions from './../../actions';
+import firebase from 'firebase';
 
 import {
   View,
@@ -12,13 +13,49 @@ import {
   PixelRatio,
   Platform
 } from 'react-native';
+import { Spinner } from './../../components/common';
 import ImagePicker from 'react-native-image-picker';
+import RNFetchBlob from 'react-native-fetch-blob';
+
+// Prepare Blob support
+const Blob = RNFetchBlob.polyfill.Blob
+const fs = RNFetchBlob.fs
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+window.Blob = Blob
+
+const uploadImage = (uri, mime = 'application/octet-stream') => {
+  const storage = firebase.storage(); //declare storage here just for this instance
+  return new Promise((resolve, reject) => {
+    const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
+    const sessionId = new Date().getTime()
+    let uploadBlob = null
+    const imageRef = storage.ref('images').child(`${sessionId}`)
+
+    fs.readFile(uploadUri, 'base64')
+      .then((data) => {
+        return Blob.build(data, { type: `${mime};BASE64` })
+      })
+      .then((blob) => {
+        uploadBlob = blob
+        return imageRef.put(blob, { contentType: mime })
+      })
+      .then(() => {
+        uploadBlob.close()
+        return imageRef.getDownloadURL()
+      })
+      .then((url) => {
+        resolve(url)
+      })
+      .catch((error) => {
+        reject(error)
+    })
+  })
+}
 
 class Profile extends Component {
-
   state = {
-    avatarSource: null,
-  }
+    uploadURL: null
+  };
 
   selectPhotoTapped() {
     const options = {
@@ -40,21 +77,13 @@ class Profile extends Component {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       } else {
-        var source;
-
-        if (Platform.OS === 'android') {
-          source = {uri: response.uri, isStatic: true};
-        } else {
-          source = {uri: response.uri.replace('file://', ''), isStatic: true};
-        }
-
-        console.log(source)
-
-        this.setState({
-          avatarSource: source
-        });
+        this.setState({ uploadURL: '' })
+        uploadImage(response.uri)
+          .then(url => this.setState({ uploadURL: url}))
+          .catch(error => console.log(error))
       }
-    })
+    });
+
   }
 
   signOut() {
@@ -70,11 +99,19 @@ class Profile extends Component {
           <View style={[centerEverything, avatarContentContainer]}>
             <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
               <View style={[centerEverything, avatarContainer, avatar]}>
-                { this.state.avatarSource === null
-                   ?
-                  <Text style={{ fontSize: 12 }}>Upload avatar</Text>
-                   :
-                  <Image style={avatar} source={this.state.avatarSource} />
+                {
+                  (() => {
+                    switch (this.state.uploadURL) {
+                      case null:
+                        return <Text style={{ fontSize: 12 }}>Upload avatar</Text>;
+                      case '':
+                        return <Spinner />
+                      default:
+                        return (
+                          <Image style={avatar} source={{uri: this.state.uploadURL}} />
+                        )
+                    }
+                  })()
                 }
               </View>
             </TouchableOpacity>
